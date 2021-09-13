92b2d11810e8dfe96f95caaf6f05a289cee0136c
Krarilotus <51748815+Krarilotus@users.noreply.github.com>
2021-09-13 22:46:58 +0200

    remove_mapsExtreme_ucpinstall

--- installSetup.bat
+++ installSetup.bat
@@ -1,6 +1,6 @@
 set "crt_dir=%~dp0"
 for %%I in ("%crt_dir%\..") do set "installPath=%%~fI"
 cd DONTOPEN
-UnofficialCrusaderPatchCLI.exe --no-output "--mapsExtreme=mapsExtreme" "--language=1" "--path=%installPath%"
+UnofficialCrusaderPatchCLI.exe --no-output "--language=1" "--path=%installPath%"
 cd ..
 mod.exe
