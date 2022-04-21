515d5b5a0b03f25b079e0a6836e111af6aaeed57
Altaruss28 <73481189+Altaruss28@users.noreply.github.com>
2022-04-21 11:03:05 +0200

    Target folder name change adjustment

--- installSetup.bat
+++ installSetup.bat
@@ -1,6 +1,6 @@
 set "crt_dir=%~dp0"
 for %%I in ("%crt_dir%\..") do set "installPath=%%~fI"
-cd DONTOPEN
+cd DoNotOpen
 UnofficialCrusaderPatchCLI.exe --no-output "--language=1" "--path=%installPath%"
 cd ..
 mod.exe
