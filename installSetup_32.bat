b99745b65379f6fad1d1557ea524f09a0e0b937c
CIO61 <enst61@gmail.com>
2022-08-17 13:42:00 +0300

    Delete installSetup_32.bat

--- installSetup_32.bat
+++
@@ -1,6 +0,0 @@
-set "crt_dir=%~dp0"
-for %%I in ("%crt_dir%\..") do set "installPath=%%~fI"
-cd DONTOPEN
-UnofficialCrusaderPatchCLI.exe --no-output "--language=1" "--path=%installPath%"
-cd ..
-mod_32.exe
