3c7de8e032b552a9dda47197088b077824a3777a
Krarilotus <51748815+Krarilotus@users.noreply.github.com>
2023-10-15 10:37:43 +0200

    ascension preview commit

--- installSetup.bat
+++ installSetup.bat
@@ -1,10 +1,10 @@
 set "crt_dir=%~dp0"
 for %%I in ("%crt_dir%\..") do set "installPath=%%~fI"
-cd DONTOPEN
+cd UCP
 UnofficialCrusaderPatchCLI.exe --no-output "--language=1" "--path=%installPath%"
 cd ..
 if defined ProgramFiles(x86) (
     mod.exe
 ) else (
-    mod_32.exe
+    mod.exe
 )
